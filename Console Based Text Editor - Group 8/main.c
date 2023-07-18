363ea172469ef3f66ff597677ac4bc7f2b5d3cdd
Julian Cumming <Jcumming5372@conestogac.on.ca>
2023-07-18 15:38:34 -0400

    added prototype

--- Console Based Text Editor - Group 8/main.c
+++ Console Based Text Editor - Group 8/main.c
@@ -1,7 +1,62 @@
+#include "uistuff.h"
+#include "backend.h"	
 
-
-
-int main(int argc, char* argv[])
+int main(void)
 {
-	return 0;
+	printf("\nWelcome to command line text editor! Options below, enter number associated with desired option.");
+	srand(time(0));
+	FILE* fp = fopen(FILELIST, "r");
+	if (!fp)
+		createfiledirectory();
+	if (fp)
+		fclose(fp);
+	bool run = true;
+	while (run)
+	{
+		Mainmenu();
+		int in = getmenuinput();
+		switch (in) {
+		case 1:
+		{
+			createfile();
+			break;
+		}
+		case 2:
+		{
+			printfiles();
+			break;
+		}
+		case 3:
+		{
+			openfile();
+			break;
+		}
+		case 4:
+		{
+			deletefile();
+			break;
+		}
+		case 5:
+		{
+			displayfile();
+			break;
+		}
+		case 6:
+		{
+			printf("\nNot yet in use\n");
+			break;
+		}
+		case 7:
+		{
+			run = false;
+			break;
+		}
+		default:
+		{
+			printf("\nenter a valid number dipshit");
+			break;
+		}
+		}
+	}
+	atexit((*programexit));
 }
