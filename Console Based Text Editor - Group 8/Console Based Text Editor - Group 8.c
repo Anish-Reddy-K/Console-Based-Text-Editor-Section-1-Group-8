51609854f353c50350a5caaea86840b5a950450b
Julian Cumming <Jcumming5372@conestogac.on.ca>
2023-07-18 14:15:39 -0400

    added basic structure

--- Console Based Text Editor - Group 8/Console Based Text Editor - Group 8.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-
-// Console Based Text Editor - Group 8.cpp : This file contains the 'main' function. Program execution begins and ends there.
-
-int main()
-{
-    printf("Hello World!\n");
-}
